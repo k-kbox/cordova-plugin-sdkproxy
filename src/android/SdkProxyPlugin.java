839765cca06d5d9bcdaab4b1570ebb783f4202ef
jun.huang <huang@guru-tech.cn>
2018-08-29 12:44:06 +0800

    1、fix bugs

--- src/android/SdkProxyPlugin.java
+++ src/android/SdkProxyPlugin.java
@@ -51,7 +51,7 @@ public class SdkProxyPlugin extends CordovaPlugin {
     private void init(CallbackContext callbackContext) {
         com.myapp.sdkproxy.SdkProxy.init(cordova.getActivity());
         String appid = com.myapp.sdkproxy.SdkProxy.getAppid();
-        String chid = com.myapp.sdkproxy.Sdkproxy.getChid();
+        String chid = com.myapp.sdkproxy.SdkProxy.getChid();
         JSONObject json = new JSONObject();
         json.put("appid", appid);
         json.put("chid", chid);
